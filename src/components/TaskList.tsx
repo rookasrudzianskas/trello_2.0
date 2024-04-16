9f3efe5f9d43bf92f0173e0e4879ba116dd3f3bd
rokas <rokas@rokas.com>
2024-04-16 15:27:29 +0300

    Update dragging behavior and task list rendering

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -37,8 +37,15 @@ export default function TaskList() {
       {/* The list of tasks */}
       <FlatList
         data={tasks}
-        contentContainerStyle={{ gap: 5 }}
-        renderItem={({ item }) => <TaskListItem task={item} />}
+        renderItem={({ item, index }) => (
+          // @ts-ignore
+          <TaskListItem task={item} index={index} />
+        )}
+        // CellRendererComponent={({ children, index }) => (
+        //   <View onLayout={(event) => console.log(event.nativeEvent.layout)}>
+        //     {children}
+        //   </View>
+        // )}
       />
 
       {/* New task input */}
--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -5,10 +5,19 @@ import { Task } from '../models/Task';
 import { useRealm } from '@realm/react';
 import { useDraggingContext } from './TaskDragArea';
 
-export default function TaskListItem({ task }: { task: Task }) {
+export const ItemHeight = 60;
+
+export default function TaskListItem({
+                                       task,
+                                       index,
+                                     }: {
+  task: Task;
+  index: number;
+}) {
   const realm = useRealm();
 
   const { setDraggingTask } = useDraggingContext();
+
   const deleteTask = () => {
     realm.write(() => {
       realm.delete(task);
@@ -16,28 +25,36 @@ export default function TaskListItem({ task }: { task: Task }) {
   };
 
   return (
-    <Link href={`/${task._id}`} asChild>
-      <Pressable
-        style={styles.container}
-        onLongPress={() => setDraggingTask(task._id)}
-      >        <Text style={styles.text}>
-          {/* @ts-ignore */}
-          {task.position}: {task.description}
-        </Text>
+    <View style={styles.root}>
+      <Link href={`/${task._id}`} asChild>
+        <Pressable
+          style={styles.container}
+          onLongPress={() => setDraggingTask(task._id, index * ItemHeight + 73)}
+        >
+          <Text style={styles.text}>
+            {task.position}: {task.description}
+          </Text>
 
-        <AntDesign onPress={deleteTask} name="close" size={16} color="gray" />
-      </Pressable>
-    </Link>
+          <AntDesign onPress={deleteTask} name="close" size={16} color="gray" />
+        </Pressable>
+      </Link>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    height: ItemHeight,
+    paddingVertical: 3,
+  },
   container: {
     backgroundColor: '#1D2125',
     padding: 15,
     borderRadius: 5,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
   },
   text: {
     color: 'white',
